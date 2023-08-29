6763043a675cbe9790d7025e630212ad25d2f5db
Justino Neutrino <justinoneutrino@gmail.com>
2023-08-29 20:16:11 +0800

    Initialised project with getComputerChoice function

---
+++ RPS.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    let guess = Math.floor(Math.random() * 3);
+    
+    if (guess === 0) {
+        return 'Rock';
+    }
+    else if (guess === 1) {
+        return "Paper";
+    }
+    else {
+        return 'Scissors';
+    }
+}
+
+console.log(getComputerChoice());
